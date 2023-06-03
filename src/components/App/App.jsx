139d20ec3280e091f02d7efb12763e1353ee60d7
Maksym Kotyk <mxm.kotyk@gmail.com>
2023-06-03 10:01:10 +0200

    Finished

---
+++ src/components/App/App.jsx
@@ -0,0 +1,18 @@
+import { Header } from 'components';
+import { CountrySearch, Home, Country } from 'pages';
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+export const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Header />}>
+          <Route index element={<Home />} />
+          <Route path="/country" element={<CountrySearch />} />
+          <Route path="/country/:countryId" element={<Country />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </>
+  );
+};
---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { useEffect, useState } from 'react';
+import { getCountries } from '../service/country-service';
+
+export const Home = () => {
+  const [countries, setCoutries] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchCoutries = async () => {
+      setIsLoading(true);
+      try {
+        const data = await getCountries();
+        setCoutries(data);
+        console.log(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchCoutries();
+  }, []);
+
+  return (
+    <Section>
+      <Container>
+        {error && (
+          <Heading>Sorry, something went wrong. Server says {error}</Heading>
+        )}
+        {isLoading && <Loader />}
+        <CountryList countries={countries} />
+      </Container>
+    </Section>
+  );
+};
